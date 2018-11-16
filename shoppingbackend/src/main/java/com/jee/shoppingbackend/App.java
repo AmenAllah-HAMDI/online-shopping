6c8fdd274e1d0965dec9865fe11a31b44c8bc9c2
AmenAllah HAMDI <amenhamdi4@gmail.com>
2018-11-15 19:30:41 -1100

    My project setup is completed successfully

---
+++ shoppingbackend/src/main/java/com/jee/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.jee.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
